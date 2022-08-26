313522a5ef90526e7ec93a978e1369b800d1b3af
Junhe Jiang <jjh_steed@163.com>
2022-08-26 11:27:38 +0800

    defered

---
+++ Java/java-series/src/main/java/com/cmg/java/scenario/defered/AsyncStarter.java
@@ -0,0 +1,13 @@
+package com.cmg.java.scenario.defered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncStarter.class, args);
+    }
+
+}

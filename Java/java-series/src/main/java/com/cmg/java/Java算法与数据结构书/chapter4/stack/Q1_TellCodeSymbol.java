961eb8b8b779661b6c6333d2561566e2f2e52416
Junhe Jiang <jjh_steed@163.com>
2022-12-05 15:10:10 +0800

    stack

---
+++ Java/java-series/src/main/java/com/cmg/java/Java算法与数据结构书/chapter4/stack/Q1_TellCodeSymbol.java
@@ -0,0 +1,15 @@
+package com.cmg.java.Java算法与数据结构书.chapter4.stack;
+
+/**
+ * 编译器区分代码(){}[]成对
+ * 时间复杂度O(n)
+ * 空间复杂度O(n)
+ */
+public class Q1_TellCodeSymbol {
+
+    //每次读入一个字符 如果字符是一个开分隔符{
+    //入栈
+    //如果读入一个闭分隔符 出栈与之比较
+    //匹配继续解析字符串
+    //不匹配
+}
